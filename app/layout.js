8f639b844e4bce44a22e4c443dc47a6f42fc12c8
srishti soni <srishtisoni7541@gmail.com>
2025-06-24 19:17:29 +0530

    remove locomotive

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SmoothScrollWrapper from "./components/ClientWrapper";
-// import ClientWrapper from "./components/ClientWrapper"; 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +22,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
     
-        <SmoothScrollWrapper>
+    
             {children}
-        </SmoothScrollWrapper>
+     
 
       </body>
     </html>
